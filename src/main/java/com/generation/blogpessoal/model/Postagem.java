6cc402a215c542cdf6749372dc0574ccc208f315
Gustavo <gustavossantos41@outlook.com>
2022-03-25 16:11:52 -0300

    Segunda parte do projeto do Blog Pessoal

--- src/main/java/com/generation/blogpessoal/model/Postagem.java
+++ src/main/java/com/generation/blogpessoal/model/Postagem.java
@@ -1,6 +1,6 @@
 package com.generation.blogpessoal.model;
 
-import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -33,7 +33,7 @@ public class Postagem {
 	
 	
 	@UpdateTimestamp
-	private LocalDate data;
+	private LocalDateTime data;
 
 	public Long getId() {
 		return id;
@@ -59,11 +59,11 @@ public class Postagem {
 		this.texto = texto;
 	}
 
-	public LocalDate getData() {
+	public LocalDateTime getData() {
 		return data;
 	}
 
-	public void setData(LocalDate data) {
+	public void setData(LocalDateTime data) {
 		this.data = data;
 	}
 	
